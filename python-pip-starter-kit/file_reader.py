14df3a5146fa7ef5a2b476b29d8593e19efb7d7e
Akash Kalpattu <srinivasan.kalpattu@student.adelaide.edu.au>
2023-07-27 14:56:36 +0930

    Added functionality for reader class

--- python-pip-starter-kit/file_reader.py
+++ python-pip-starter-kit/file_reader.py
@@ -1,11 +1,20 @@
+import gman
+
 class File_reader():
     def __init__(self):
-        pass
+        self.__remaining_power = 200
 
 
     def read_file(self, file):
         self.__lines = self.__file.readlines()
 
         for self.__line in self.__lines:
-            pass
-            # Add logic
+            # Format lines, remove the newline and split into a list
+            self.__line = self.__line.replace("\n", "").split(" ")
+
+            if self.__line[0] == "SOURCE":
+                gman = gman.Gman(self.__line[1], self.__line[2], self.__line[3])
+            elif self.__line[0] == "DESTINATION":
+                self.__remaining_power = gman.go_to(self.__line[1], self.__line[2])
+            elif self.__line[0] == "PRINT_POWER":
+                print(self.__remaining_power)
