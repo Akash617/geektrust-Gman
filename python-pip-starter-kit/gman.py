65337829d8e6ff0f71472e2ea9c3477406b015ab
Akash Kalpattu <srinivasan.kalpattu@student.adelaide.edu.au>
2023-07-27 14:52:32 +0930

    Added functionality for Gman class

--- python-pip-starter-kit/gman.py
+++ python-pip-starter-kit/gman.py
@@ -5,5 +5,55 @@ class Gman():
         self.__direction = direction
         self.__power = 200
 
+
+    def on_target(self, x, y):
+        if self.__x != x or self.__y != y:
+            return False
+
+        return True
+
+
+    def turn(self, opposite):
+        if opposite == "EW":
+            if (y - self.__y) < 0:
+                self.__direction = "S"
+                self.__power -= 5
+            elif (y - self.__y) > 0:
+                self.__direction = "N"
+                self.__power -= 5
+        elif opposite == "NS":
+            if (x - self.__x) < 0:
+                self.__direction = "W"
+                self.__power -= 5
+            elif (x - self.__x) > 0:
+                self.__direction = "E"
+                self.__power -= 5
+
+
+    def move(self, x, y):
+        direction_assocation = {"N": (0,1), "E": (1,0), "S": (0,-1), "W": (-1,0)}
+
+        self.__x += direction_assocation[self.__direction][0]
+        self.__y += direction_assocation[self.__direction][1]
+
+        self.__power -= 10
+
+
+    def turn_or_move(self, x, y):
+        if self.__direction == "E" and (x - self.__x) <= 0:
+            turn("EW")
+        elif self.__direction == "W" and (x - self.__x) >= 0:
+            turn("EW")
+        elif self.__direction == "N" and (y - self.__y) <= 0:
+            turn("NS")
+        elif self.__direction == "S" and (y - self.__y) >= 0:
+            turn("NS")
+        else:
+            move(x, y)
+
+
     def go_to(self, x, y):
-        pass
+        while not on_target(x, y):
+            turn_or_move(x, y)
+
+        return self.__power
