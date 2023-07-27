8bca1d3c810cb5d70576d3de2cb31dcd8a157c16
Akash Kalpattu <srinivasan.kalpattu@student.adelaide.edu.au>
2023-07-27 18:12:16 +0930

    Added another class to handle the actions

--- python-pip-starter-kit/file_reader.py
+++ python-pip-starter-kit/file_reader.py
@@ -1,4 +1,4 @@
-import gman
+import controller
 
 class File_reader():
     def __init__(self, file):
@@ -9,15 +9,5 @@ class File_reader():
     def read_file(self):
         self.__lines = self.__file.readlines()
 
-        # Iterate over lines in the file
-        for self.__line in self.__lines:
-            # Format lines, remove the newline and split into a list
-            self.__line = self.__line.replace("\n", "").split(" ")
-
-            # Create a Gman class at the source point
-            if self.__line[0] == "SOURCE":
-                controller_gman = gman.Gman(int(self.__line[1]), int(self.__line[2]), self.__line[3])
-            elif self.__line[0] == "DESTINATION":
-                self.__remaining_power = controller_gman.go_to_target(int(self.__line[1]), int(self.__line[2]))
-            elif self.__line[0] == "PRINT_POWER":
-                print("POWER " + str(self.__remaining_power))
+        self.__controller = controller.Controller()
+        self.__controller.handle(self.__lines)
