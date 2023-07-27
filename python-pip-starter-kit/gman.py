598da681847cad016dc74656949e7da5f17bab53
Akash Kalpattu <srinivasan.kalpattu@student.adelaide.edu.au>
2023-07-27 17:56:58 +0930

    Changed some names, not really needed I think geektrust thinks x, y are placeholders..

--- python-pip-starter-kit/gman.py
+++ python-pip-starter-kit/gman.py
@@ -1,7 +1,7 @@
 class Gman():
-    def __init__(self, x, y, direction):
-        self.__x = x
-        self.__y = y
+    def __init__(self, x_coordinates, y_coordinates, direction):
+        self.__x_coordinates = x_coordinates
+        self.__y_coordinates = y_coordinates
         self.__direction = direction
         self.__power = 200
         self.__cost_to_turn = 5
@@ -9,24 +9,24 @@ class Gman():
 
 
     # Check if Gman is on target or not
-    def on_target(self, x, y):
-        if self.__x != x or self.__y != y:
+    def is_on_target(self, x_coordinates, y_coordinates):
+        if self.__x_coordinates != x_coordinates or self.__y_coordinates != y_coordinates:
             return False
 
         return True
 
 
     # Turn Gman 90 degrees to face a direction he needs to go
-    def turn(self, opposite, x, y):
+    def turn(self, opposite, x_coordinates, y_coordinates):
         if opposite == "EW":
-            if (y - self.__y) < 0:
+            if (y_coordinates - self.__y_coordinates) < 0:
                 self.__direction = "S"
                 self.__power -= self.__cost_to_turn
             else:
                 self.__direction = "N"
                 self.__power -= self.__cost_to_turn
         elif opposite == "NS":
-            if (x - self.__x) < 0:
+            if (x_coordinates - self.__x_coordinates) < 0:
                 self.__direction = "W"
                 self.__power -= self.__cost_to_turn
             else:
@@ -35,32 +35,32 @@ class Gman():
 
 
     # Move the Gman by 1 step in the direction he's facing
-    def move(self, x, y):
+    def move(self, x_coordinates, y_coordinates):
         direction_assocation = {"N": (0,1), "E": (1,0), "S": (0,-1), "W": (-1,0)}
 
-        self.__x += direction_assocation[self.__direction][0]
-        self.__y += direction_assocation[self.__direction][1]
+        self.__x_coordinates += direction_assocation[self.__direction][0]
+        self.__y_coordinates += direction_assocation[self.__direction][1]
 
         self.__power -= self.__cost_to_move
 
 
     # Decide if the Gman needs to turn or if he can move in the direction he's facing
-    def turn_or_move(self, x, y):
-        if self.__direction == "E" and (x - self.__x) <= 0:
-            self.turn("EW", x, y)
-        elif self.__direction == "W" and (x - self.__x) >= 0:
-            self.turn("EW", x, y)
-        elif self.__direction == "N" and (y - self.__y) <= 0:
-            self.turn("NS", x, y)
-        elif self.__direction == "S" and (y - self.__y) >= 0:
-            self.turn("NS", x, y)
+    def turn_or_move(self, x_coordinates, y_coordinates):
+        if self.__direction == "E" and (x_coordinates - self.__x_coordinates) <= 0:
+            self.turn("EW", x_coordinates, y_coordinates)
+        elif self.__direction == "W" and (x_coordinates - self.__x_coordinates) >= 0:
+            self.turn("EW", x_coordinates, y_coordinates)
+        elif self.__direction == "N" and (y_coordinates - self.__y_coordinates) <= 0:
+            self.turn("NS", x_coordinates, y_coordinates)
+        elif self.__direction == "S" and (y_coordinates - self.__y_coordinates) >= 0:
+            self.turn("NS", x_coordinates, y_coordinates)
         else:
-            self.move(x, y)
+            self.move(x_coordinates, y_coordinates)
 
 
     # Move or turn Gman until he reaches the target and return the remaining power
-    def go_to(self, x, y):
-        while not self.on_target(x, y):
-            self.turn_or_move(x, y)
+    def go_to_target(self, x_coordinates, y_coordinates):
+        while not self.on_target(x_coordinates, y_coordinates):
+            self.turn_or_move(x_coordinates, y_coordinates)
 
         return self.__power
